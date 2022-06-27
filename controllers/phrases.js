fccd003c401ec060ffeb3e3e28ae4b045c30f661
ehud <ehudmi12@gmail.com>
2022-06-28 01:16:22 +0300

    working on the logic

--- controllers/phrases.js
+++ controllers/phrases.js
@@ -5,9 +5,28 @@ const bodyParser = require("body-parser");
 let wordArray = [];
 let replyArray = [];
 let currentBoxAnswer;
+let finalAnswer;
+
+const whFun = async (type) => {
+  await boxAnswer(type);
+  secondWord(wordArray[1]);
+  if (currentBoxAnswer.type == "general") {
+    finalAnswer = currentBoxAnswer.response;
+    console.log(finalAnswer);
+    return finalAnswer;
+  } else {
+    finalAnswer = `${replyArray.join(" ")} ${currentBoxAnswer.response}`;
+    console.log(finalAnswer);
+    return finalAnswer;
+  }
+};
 
 const splitSentence = (sent) => {
   wordArray = sent.split(" ");
+  wordArray[wordArray.length - 1] = wordArray[wordArray.length - 1].replace(
+    "?",
+    ""
+  );
   console.log(wordArray);
   firstWord(wordArray[0]);
 };
@@ -15,69 +34,98 @@ const splitSentence = (sent) => {
 const firstWord = (word) => {
   switch (word) {
     case "who":
-      secondWord(wordArray[1]);
-      boxAnswer("who");
-      console.log(replyArray, currentBoxAnswer);
+      whFun("who");
       break;
     case "what":
+      whFun("what");
       break;
     case "where":
+      whFun("where");
       break;
     case "when":
+      whFun("when");
       break;
     case "why":
+      whFun("why");
       break;
     case "how":
+      whFun("how");
       break;
   }
 };
 
 const secondWord = (word) => {
+  thirdWord(wordArray[2]);
   switch (word) {
     case "is":
-      thirdWord(wordArray[2]);
+      // thirdWord(wordArray[2]);
       replyArray.push("is");
       break;
     case "are":
-      thirdWord(wordArray[2]);
+      // thirdWord(wordArray[2]);
       replyArray.push("are");
       break;
     case "do":
-      thirdWord(wordArray[2]);
+      // thirdWord(wordArray[2]);
       break;
+    case "am":
+      // thirdWord(wordArray[2]);
+      replyArray.push("are");
+      break;
+    default:
+      // thirdWord(wordArray[2]);
+      replyArray.splice(1, word);
   }
 };
 
 const thirdWord = (word) => {
   switch (word) {
     case "my":
-      replyArray = wordArray.slice(3, wordArray.length - 1);
+      replyArray = wordArray.slice(3, wordArray.length);
       replyArray.unshift("your");
       break;
     case "your":
-      replyArray = wordArray.slice(3, wordArray.length - 1);
+      replyArray = wordArray.slice(3, wordArray.length);
       replyArray.unshift("my");
       break;
     case "the":
-      replyArray = wordArray.slice(2, wordArray.length - 1);
+      replyArray = wordArray.slice(2, wordArray.length);
       break;
+    case "I":
+      replyArray = ["you"];
+      break;
+    default:
+      replyArray = wordArray.slice(3, wordArray.length);
+      replyArray.unshift(word);
   }
 };
 
-const boxAnswer = (type) => {
-  _boxAnswer(type)
-    .then((result) => {
-      let allAnswers = result;
-      let index = Math.floor(Math.abs(Math.random() * (allAnswers.length - 1)));
-      currentBoxAnswer = allAnswers[index];
-      console.log(currentBoxAnswer);
-      // res.json(result);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(404).json({ msg: "not found" });
-    });
+const boxAnswer = async (type) => {
+  try {
+    result = await _boxAnswer(type);
+    let allAnswers = result;
+    let index = Math.floor(Math.abs(Math.random() * (allAnswers.length - 1)));
+    currentBoxAnswer = allAnswers[index];
+    console.log(index);
+  } catch (error) {
+    console.log(error);
+  }
 };
+
+// const boxAnswer = (type) => {
+//   _boxAnswer(type)
+//     .then((result) => {
+//       let allAnswers = result;
+//       let index = Math.floor(Math.abs(Math.random() * (allAnswers.length - 1)));
+//       currentBoxAnswer = allAnswers[index];
+//       console.log(currentBoxAnswer);
+//       // res.json(result);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(404).json({ msg: "not found" });
+//     });
+// };
 
 const askQuestion = (req, res) => {
   console.log(req.body.text);
