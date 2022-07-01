0004d5ca45b1b2da680531dc2c7480ac3c4b8401
ehud <ehudmi12@gmail.com>
2022-07-01 21:30:35 +0300

    try again

--- connections/linda-heroku-db.js
+++ connections/linda-heroku-db.js
@@ -2,7 +2,10 @@ const knex = require("knex");
 
 const db = knex({
   client: "pg",
-  connection: process.env.DATABASE_URL,
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+  },
   //   connection: {
 
   //     host: process.env.DB_HOST,
