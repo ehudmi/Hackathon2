c57232f136e9ee453dd5d9a8dd9e1cbbcdee3a99
ehud <ehudmi12@gmail.com>
2022-07-01 18:59:30 +0300

    fix routing error3

--- routes/phrases.js
+++ routes/phrases.js
@@ -1,22 +1,9 @@
 const express = require("express");
 const { askQuestion, newGoodFit } = require("../controllers/phrases.js");
-// const {
-// getProducts,
-// getProduct,
-// searchProduct,
-// askQuestion,
-// updateProduct,
-// deleteProduct,
-// } = require("../controllers/phrases.js");
 
 const router = express.Router();
 
-// router.get("/search", searchProduct);
-// router.get("/:id", getProduct);
-// router.get("/", getProducts);
-router.post("/api/phrases", askQuestion);
-router.put("/api/phrases", newGoodFit);
-// router.put("/:id", updateProduct);
-// router.delete("/:id", deleteProduct);
+router.post("/", askQuestion);
+router.put("/", newGoodFit);
 
 module.exports = router;
